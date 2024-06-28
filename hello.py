ee5c8ce4c6e47d6b5068dde6a4d6d6a50b3da1d0
Ritwick Dey <ritwickdey@outlook.com>
2024-06-28 16:29:45 +0530

    Current hour to improve greeting msg

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
+from datetime import datetime
+
+def get_current_hour() -> int:
+    now = datetime.now()
+    return now.microsecond
+
 def sayHello(name: str) -> str:
+    current_hour = get_current_hour()
+    
+    if current_hour < 12:
+        return f"Good Night, {name}!"
+    
+    if current_hour < 18:
+        return f"Good Afternoon, {name}!"
+    
     return f"Hello, {name}!"
 
 
